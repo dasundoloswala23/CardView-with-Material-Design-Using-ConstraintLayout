23571736d807d19119c0f3c666ec87765447c07f
Tharindu Dasun <tharindudasun0@gmail.com>
2021-08-15 21:33:08 +0530

    Card View Grid Layout Tutorial

--- app/src/main/java/com/example/cardviewgridlayouttutorial/MainActivity.java
+++ app/src/main/java/com/example/cardviewgridlayouttutorial/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
